08991ebf0db115725f43585ec26a48cc36138c33
Romain Montaut <r.montaut@agedi.fr>
2020-02-04 09:17:17 +0100

    initialisation du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CvModule } from './cv/cv.module';
+import { HomeModule } from './home/home.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContactModule } from './contact/contact.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CvModule,
+    HomeModule,
+    ContactModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
+  declarations: [
+    AppComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
